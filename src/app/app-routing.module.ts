baa36e830db49024b5d976b1597f7ff1a39dd8cd
Austin Gray <austin.gray64@gmail.com>
2018-09-27 14:44:54 -0700

    again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { DetailsComponent } from './details/details.component';
 import { PostsComponent } from './posts/posts.component';
